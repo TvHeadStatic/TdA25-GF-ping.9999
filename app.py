2f824899b0249b5606f82a51a90de48ed908fab0
May_Be_Alex <a.polasek01@gmail.com>
2024-11-11 13:09:04 +0100

    Fix and run ig

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Hello TdA"
+
+@app.route("/api")
+def api():
+    return "Student Cyber Games"
+
+if __name__ == '__main__':
+    app.run(debug=True)
