4f53d4da3bb45953f29a79114b364553e24aed3f
May_Be_Alex <123482369+PlatinaStar@users.noreply.github.com>
2024-11-12 07:55:13 +0100

    Uwu

--- app/game.py
+++ app/game.py
@@ -4,4 +4,4 @@ game_bp = Blueprint('game_bp', __name__, template_folder = "../HTML/templates")
 
 @game_bp.route("/game")
 def game():
-    return render_template("game.html", title = "TdA")
+    return render_template("game.html", title = "TdA"), 200
