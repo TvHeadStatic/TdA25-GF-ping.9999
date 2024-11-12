a9053acac4ecb9bf2a8939eef8ec16934e11e82b
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-12 09:07:12 +0100

    redone file structure uwu :3

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, jsonify
 
-from api import api_bp
-from game import game_bp
+from api.api import api_bp
+from game.game import game_bp
 
 app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return "Hello TdA", 200
 
 app.register_blueprint(api_bp)
 app.register_blueprint(game_bp)
--- app/game/game.py
+++ app/game/game.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, Blueprint
 
-game_bp = Blueprint('game_bp', __name__, template_folder = "../HTML/templates")
+game_bp = Blueprint('game_bp', __name__, template_folder = "./templates")
+
+@game_bp.route("/")
+def main():
+    return "Hello TdA", 200
 
 @game_bp.route("/game")
 def game():
