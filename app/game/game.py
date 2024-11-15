6050b57db709bb6145a9e92bdb33d3c4af2c3722
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-15 16:33:32 +0100

    game page + api

--- app/game/game.py
+++ app/game/game.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, request
+import requests
+import json
 
 game_bp = Blueprint('game_bp', __name__, template_folder = "./templates")
 
@@ -9,3 +11,9 @@ def main():
 @game_bp.route("/game")
 def game():
     return render_template("game.html", title = "TdA"), 200
+
+@game_bp.route("/game/<id>")
+def gaming(id):
+    apiRes = requests.get(request.url_root + "api/v1/games/" + id)
+    apiResponse = json.loads(apiRes.text)
+    return render_template("game.html", title = "TdA", gameData = apiResponse), 200
