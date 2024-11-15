9795a40e121d4d8b054e53a688bc66d4130d7673
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-15 08:51:28 +0100

    eh, post datetime

--- app/api/api_post.py
+++ app/api/api_post.py
@@ -1,9 +1,10 @@
 from flask import jsonify
 import sqlite3
 import uuid
+import datetime
 
 def api_post(cursor, req, conn):
     state = "INSERT INTO piskvorky(uuid, createdAt, updatedAt, name, difficulty, gameState, board) VALUES(?, ?, ?, ?, ?, ?, ?)"
-    cursor.execute(state, [str(uuid.uuid4), req.createdAt, req.updatedAt, req.name, req.difficulty, req.gameState, str(req.board)])
+    cursor.execute(state, [str(uuid.uuid4), str(datetime.datetime.now()), str(datetime.datetime.now()), req.name, req.difficulty, None, str(req.board)])
     conn.commit()
     return req, 201
