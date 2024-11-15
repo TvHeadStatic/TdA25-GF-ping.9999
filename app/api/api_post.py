947abf9b25ea3c5f13f817cc236b8fabfe0a0c87
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-15 08:34:05 +0100

    201????

--- app/api/api_post.py
+++ app/api/api_post.py
@@ -6,4 +6,4 @@ def api_post(cursor, req, conn):
     state = "INSERT INTO piskvorky(uuid, createdAt, updatedAt, name, difficulty, gameState, board) VALUES(?, ?, ?, ?, ?, ?, ?)"
     cursor.execute(state, [str(uuid.uuid4), req.createdAt, req.updatedAt, req.name, req.difficulty, req.gameState, str(req.board)])
     conn.commit()
-    return jsonify(req), 200
+    return jsonify(req), 201
