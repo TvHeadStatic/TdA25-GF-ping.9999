08c0f6c9353de0f4e0a36f4a3acae5844edf27de
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-15 09:04:23 +0100

    Update api_post.py

--- app/api/api_post.py
+++ app/api/api_post.py
@@ -5,6 +5,6 @@ import datetime
 
 def api_post(cursor, req, conn):
     state = "INSERT INTO piskvorky(uuid, createdAt, updatedAt, name, difficulty, gameState, board) VALUES(?, ?, ?, ?, ?, ?, ?)"
-    cursor.execute(state, [str(uuid.uuid4), str(datetime.datetime.now()), str(datetime.datetime.now()), req.name, req.difficulty, None, str(req.board)])
+    cursor.execute(state, [str(uuid.uuid4), str(datetime.datetime.now()), str(datetime.datetime.now()), req["name"], req["difficulty"], None, str(req["board"])])
     conn.commit()
     return req, 201
