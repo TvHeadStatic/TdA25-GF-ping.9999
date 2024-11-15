81d89f78f2d2ec6f46e4b8291d8e0a524d711547
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-15 11:56:25 +0100

    a

--- app/api/api_post.py
+++ app/api/api_post.py
@@ -8,6 +8,6 @@ from api.db_manager import db_manager
 def api_post(req):
     dbMan = db_manager()
     state = "INSERT INTO piskvorky(uuid, createdAt, updatedAt, name, difficulty, gameState, board) VALUES(?, ?, ?, ?, ?, ?, ?)"
-    dbMan.cursor.execute(state, [str(uuid.uuid4), req.createdAt, req.updatedAt, req.name, req.difficulty, req.gameState, str(req.board)])
+    dbMan.cursor.execute(state, [str(uuid.uuid4), str(datetime.datetime.now()), str(datetime.datetime.now()), req["name"], req["difficulty"], None, str(req["board"])])
     dbMan.conn.commit()
     return req, 201
