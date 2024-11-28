e48fa95de01c61690310d9cd0ac31ca52d085ff7
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-11-28 21:21:49 +0100

    checks for size, chars and endgame
    unit tests on my pc say it is ok
    let's hope for da best uwu

--- app/api/api_post.py
+++ app/api/api_post.py
@@ -4,14 +4,7 @@ import uuid
 import datetime
 
 from api.db_manager import db_manager
-
-def validate_gamestate(board):
-    oCounter = 1
-    for y in board:
-        for x in y:
-            oCounter += int(x =="O")
-    if oCounter > 5: return "midgame"
-    return "opening"
+from api.ticktacktoe_functionality import validate_gamestate, has_invalid_char, has_illegal_size
 
 def api_post(req):
     dbMan = db_manager()
@@ -19,6 +12,10 @@ def api_post(req):
     newuuid = str(uuid.uuid4())
     createdAt = str(datetime.datetime.now())
     updatedAt = str(datetime.datetime.now())
+    if has_invalid_char(req["board"]) or has_illegal_size(req["board"]):
+        return jsonify({
+            "status": 422
+        }), 422
     gameState = validate_gamestate(req["board"])
     dbMan.cursor.execute(methodQuery, [newuuid, createdAt, updatedAt, req["name"], req["difficulty"], gameState, str(req["board"])])
     dbMan.conn.commit()
--- app/api/api_put.py
+++ app/api/api_put.py
@@ -4,12 +4,18 @@ import uuid
 import datetime
 from ast import literal_eval
 from api.db_manager import db_manager
+from api.ticktacktoe_functionality import validate_gamestate, has_invalid_char, has_illegal_size
 
 def api_put(id, req):
     dbMan = db_manager()
     methodQuery = "UPDATE piskvorky SET updatedAt = ?, name = ?, difficulty = ?, gameState = ?, board = ? WHERE uuid LIKE ?"
     updatedAt = str(datetime.datetime.now())
-    dbMan.cursor.execute(methodQuery, [updatedAt, req["name"], req["difficulty"], None, str(req["board"]), id])
+    if has_invalid_char(req["board"]) or has_illegal_size(req["board"]):
+        return jsonify({
+            "status": 422
+        }), 422
+    gameState = validate_gamestate(req["board"])
+    dbMan.cursor.execute(methodQuery, [updatedAt, req["name"], req["difficulty"], gameState, str(req["board"]), id])
     dbMan.conn.commit()
     methodQuery = "SELECT * FROM piskvorky WHERE uuid LIKE ?"
     dbMan.cursor.execute(methodQuery, [id])
