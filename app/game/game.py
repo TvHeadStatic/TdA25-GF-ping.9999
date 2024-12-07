e12ea9bc8d9563d572110f4762ba6e23bce46833
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2024-12-08 00:38:20 +0100

    added thumbnails to /game

--- app/game/game.py
+++ app/game/game.py
@@ -4,17 +4,19 @@ from ast import literal_eval
 
 game_bp = Blueprint('game_bp', __name__, template_folder = "./templates", static_folder="static", static_url_path="/")
 
+apiURL = "https://50336bc6.app.deploy.tourde.app/api/v1/games"
+
 @game_bp.route("/")
 def main():
     return render_template("index.html", title = "TdA | Home"), 200
 
 @game_bp.route("/game")
 def game():
-    apiRez = requests.get("https://50336bc6.app.deploy.tourde.app/api/v1/games")
+    apiRez = requests.get(apiURL)
     return render_template("game.html", title = "TdA | Game", gamez = apiRez.json()), 200
 
 @game_bp.route("/game/<id>")
 def gaming(id):
     # "http://" + request.url_root + "api/v1/games/"
-    apiRes = requests.get("https://50336bc6.app.deploy.tourde.app/api/v1/games/" + id)
+    apiRes = requests.get(apiURL + "/" + id)
     return render_template("board.html", title = "TdA | " + apiRes.json()["name"], gameData = apiRes.json()), 200
