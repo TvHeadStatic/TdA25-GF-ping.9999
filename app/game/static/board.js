67cba07b4ca69bb883c466273c529b20440f0fdd
May_Be_Alex <123482369+PlatinaStar@users.noreply.github.com>
2025-02-19 12:37:12 +0100

    Truss me, I'm an indženýr

--- app/game/static/board.js
+++ app/game/static/board.js
@@ -100,9 +100,14 @@ function board_edit(x, y) {
         if (winner > 0) {
             document.getElementById("wincont").innerHTML = winX
             document.getElementById("wincont2").innerHTML = winX
+            socket.emit("end_game", {"winner": "x", "x": players["X"], "o": players["O"]})
+            console.log("moans")
         } else {
             document.getElementById("wincont").innerHTML = winO
             document.getElementById("wincont2").innerHTML = winO
+            socket.emit("end_game", {"winner": "o", "x": players["X"], "o": players["O"]}) 
+            console.log("moanz")
+
         }
         document.getElementById("winScreenHolder").style.display = "block"
         fetch(`/api/gateway/${uuid}`, {
