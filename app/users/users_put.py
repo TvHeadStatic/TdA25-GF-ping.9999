33566b80776e6832739b9df02b7ba67b913393be
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2025-02-27 08:15:30 +0100

    preps for user profile updates :3

--- app/users/users_put.py
+++ app/users/users_put.py
@@ -9,10 +9,29 @@ from db.db_manager import db_manager
 def users_put(id, req):
     dbMan = db_manager()
 
-    methodQuery = "UPDATE users SET email = %s, username = %s, password = %s, salt = %s, elo = %s, wins = %s, draws = %s, losses = %s WHERE uuid LIKE %s"
-    salt = hex(random.randrange(0, 2**24))
-    newPassword = hashlib.sha256(f"{req['password']}{salt}".encode()).hexdigest()
-    dbMan.cursor.execute(methodQuery, [req["email"], req["username"], newPassword, salt, req["elo"], req["wins"], req["draws"], req["losses"]])
+    if (req["password"] != "" or req["password"] != None):
+        methodQuery = "UPDATE users SET password = %s, salt = %s WHERE uuid LIKE %s"
+        salt = hex(random.randrange(0, 2**24))
+        newPassword = hashlib.sha256(f"{req['password']}{salt}".encode()).hexdigest()
+        dbMan.cursor.execute(methodQuery, [newPassword, salt])
+    if (req["username"] != "" or req["username"] != None):
+        methodQuery = "UPDATE users SET username = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["username"]])
+    if (req["elo"] != "" or req["elo"] != None):
+        methodQuery = "UPDATE users SET elo = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["elo"]])
+    if (req["wins"] != "" or req["wins"] != None):
+        methodQuery = "UPDATE users SET wins = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["wins"]])
+    if (req["draws"] != "" or req["draws"] != None):
+        methodQuery = "UPDATE users SET draws = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["draws"]])
+    if (req["losses"] != "" or req["losses"] != None):
+        methodQuery = "UPDATE users SET losses = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["losses"]])
+    if (req["email"] != "" or req["email"] != None):
+        methodQuery = "UPDATE users SET email = %s WHERE uuid LIKE %s"
+        dbMan.cursor.execute(methodQuery, [req["email"]])
 
     methodQuery = "SELECT users.uuid, users.createdAt, users.username, users.elo, users.wins, users.draws, users.losses FROM users WHERE uuid LIKE %s"
     dbMan.cursor.execute(methodQuery, [id])
