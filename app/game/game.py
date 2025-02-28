fcc55dd94cfdf4d4d076eb6430e039db366445b2
May_Be_Alex <a.polasek01@gmail.com>
2025-02-28 12:21:27 +0100

    DarkMode

--- app/game/game.py
+++ app/game/game.py
@@ -9,14 +9,23 @@ game_bp = Blueprint('game_bp', __name__, template_folder = "./templates", static
 
 @game_bp.errorhandler(404) 
 def not_found(e): 
-    return render_template("404.html") 
+    finalPage = "404.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/404.html"
+    return render_template(finalPage) 
 
 @game_bp.route("/")
 def main():
-    return render_template("index.html", title = "TdA | Home"), 200
+    finalPage = "index.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/index.html"
+    return render_template(finalPage, title = "TdA | Home"), 200
 
 @game_bp.route("/game")
 def game():
+    finalPage = "game.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/game.html"
     heks = [
         'Did you know, "Tick Tack Toe" has nothing to do with putting tic-tacs between your toes?',
         'Did you know, that if you took out your organs and put them in a line over the equator, you\'d be dead?',
@@ -33,11 +42,14 @@ def game():
         if "isguest" in session["user"]:
             session.pop("user", None)
             return redirect(url_for("game_bp.main"))
-        return render_template("game.html", title = "TdA | Game", userData = session["user"], hek = random.choice(heks)), 200
+        return render_template(finalPage, title = "TdA | Game", userData = session["user"], hek = random.choice(heks)), 200
     return redirect(url_for("game_bp.main"))
 
 @game_bp.route("/game/canvas")
 def gamemaking():
+    finalPage = "board_canvas.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/board_canvas.html"
     if "user" in session:
         gameDat = {
         "board": [
@@ -61,20 +73,26 @@ def gamemaking():
                 "gameState": "opening",
                 "name": "",
         }
-        return render_template("board_canvas.html", title = "TdA | New Project", gameData = gameDat, userData = session["user"]), 200
+        return render_template(finalPage, title = "TdA | New Project", gameData = gameDat, userData = session["user"]), 200
     return redirect(url_for("game_bp.main"))
 
 @game_bp.route("/game/<id>")
 def gaming(id):
+    finalPage = "board.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/board.html"
     if "user" in session:
         apiRes = requests.get(url_for("api_bp.api", id=id, _external=True))
         if apiRes.status_code != 200:
             return redirect("/404")
-        return render_template("board.html", title = "TdA | " + apiRes.json()["name"], gameData = apiRes.json(), userData = session["user"]), 200
+        return render_template(finalPage, title = "TdA | " + apiRes.json()["name"], gameData = apiRes.json(), userData = session["user"]), 200
     return redirect(url_for("game_bp.main"))
 
 @game_bp.route("/freeplay")
 def free_of_charge():
+    finalPage = "board.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/board.html"
     if "code" not in request.args:
         return redirect("/404")
     apiRes = requests.get(url_for("api_bp.api", id=request.args.get("code"), _external=True))
@@ -92,10 +110,19 @@ def free_of_charge():
                 "token": str(random.randrange(0, 2**24)),
                 "isguest": True
         }
-    return render_template("board.html", title = "TdA | " + apiRes.json()["name"], gameData = apiRes.json(), userData = session["user"]), 200
+    return render_template(finalPage, title = "TdA | " + apiRes.json()["name"], gameData = apiRes.json(), userData = session["user"]), 200
 
 @game_bp.route("/game/user/<id>")
 def profiling(id):
+    finalPage = "user_profile.html"
+    finalAPage = "admin_profile.html"
+    finalMPage = "mein_user_profile.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/user_profile.html"
+        finalAPage = "dark_mode/admin_profile.html"
+        finalMPage = "dark_mode/mein_user_profile.html"
+
+
     if "user" in session:
         dbMan = db_manager()
         methodQuery = "SELECT username, losses, wins, draws, uuid, elo, createdat, email, \"gameHistory\" FROM users WHERE uuid LIKE %s"
@@ -112,12 +139,15 @@ def profiling(id):
         return redirect(url_for("game_bp.main"))
     if session["user"]["uuid"] == result['uuid']:
         if result['email'] == "tda@scg.cz":
-            return render_template("admin_profile.html", title = "TdA | " + result["username"], userData = result, allUserData = result2), 200
-        return render_template("mein_user_profile.html", title = "TdA | " + result["username"], userData = result), 200
-    return render_template("user_profile.html", title = "TdA | " + result["username"], userData = result), 200
+            return render_template(finalAPage, title = "TdA | " + result["username"], userData = result, allUserData = result2), 200
+        return render_template(finalMPage, title = "TdA | " + result["username"], userData = result), 200
+    return render_template(finalPage, title = "TdA | " + result["username"], userData = result), 200
 
 @game_bp.route("/game/leaderboard/<id>")
 def jacking(id):
+    finalPage = "leaderboard.html"
+    if "darkmode" in request.args:
+        finalPage = "dark_mode/leaderboard.html"
     dbMan = db_manager()
     match(id):
         case "losses":
@@ -129,10 +159,13 @@ def jacking(id):
     dbMan.cursor.execute(methodQuery)
     result = dbMan.cursor.fetchall()
     dbMan.free()
-    return render_template("leaderboard.html", title = "TdA | Leaderboard " + id, boardData = result, orderedby = id), 200
+    return render_template(finalPage, title = "TdA | Leaderboard " + id, boardData = result, orderedby = id), 200
 
 @game_bp.route("/game/matchmaking")
 def matchumakingu():
+    yoMom = ""
+    if "darkmode" in request.args:
+        yoMom = "?darkmode=true"
     dbMan = db_manager()
     methodQuery = "SELECT elo FROM users WHERE uuid LIKE %s"
     dbMan.cursor.execute(methodQuery, [session["user"]["uuid"]])
@@ -171,6 +204,6 @@ def matchumakingu():
 
     if len(result) == 0:
         r = requests.post(url_for("api_bp.api_getall", _external=True), json = gameDat)
-        return redirect(url_for("game_bp.gaming", id=r.json()["uuid"]))
+        return redirect(str(url_for("game_bp.gaming", id=r.json()["uuid"])+yoMom))
     else:
-        return redirect(url_for("game_bp.gaming", id=result[0]["uuid"]))
+        return redirect(str(url_for("game_bp.gaming", id=result[0]["uuid"])+yoMom))
