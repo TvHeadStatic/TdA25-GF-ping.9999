2b45e9c5064928a774d16287828f21cf3651f480
RngValue <bezmesiaca@seznam.cz>
2025-03-21 18:52:46 +0100

    hey now you're a rockstar

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from werkzeug.middleware.proxy_fix import ProxyFix
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
+messages = []
 
 if os.path.isfile(".env"): load_dotenv()
 
@@ -19,7 +20,7 @@ socketio.init_app(app, cors_allowed_origins="*")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", heynow=messages)
 
 @app.route("/features")
 def features():
@@ -28,6 +29,12 @@ def features():
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data["data"])
+
+@socketio.on('send_message')
+def send_message(data):
+    messages.append(data["text"])
+    print(data["text"])
+    emit("receive", data["text"], broadcast=True, include_self=True)
 
 print("API_SECRET env test (not actual key don' worry): " + str(os.getenv("API_SECRET")))
 if __name__ == '__main__':
--- uf_docker.sh
+++ uf_docker.sh
@@ -1,2 +1,2 @@
 docker build . -t myapp --build-arg "PORT=3000"
-ocker run --rm -p 3000:3000 -t myapp
+docker run --rm -p 3000:3000 -t myapp
