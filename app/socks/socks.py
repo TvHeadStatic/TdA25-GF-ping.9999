62f08e07cd79e5512768271b8a81f3b2ffdeab3a
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2025-03-22 09:07:00 +0000

    :3

--- app/socks/socks.py
+++ app/socks/socks.py
@@ -5,8 +5,18 @@ messages = []
 socketio = SocketIO()
 
 @socketio.on('join_room')
-def join_room(data):
-    print('received message: ' + data["data"])
+def join_me(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    print(username + "joined room " + room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    print(username + "joined room " + room)
 
 @socketio.on('send_message')
 def send_message(data):
